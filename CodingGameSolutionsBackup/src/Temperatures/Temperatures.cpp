3b84a7c2e23a371d15b427dd27220542a2b62157
JotaPe <jcamacho249@unab.edu.co>
2024-09-30 20:12:59 -0500

    [Initial Commit] - Cpp - Google Test template

---
+++ CodingGameSolutionsBackup/src/Temperatures/Temperatures.cpp
@@ -0,0 +1,8 @@
+﻿#include "Temperatures.h"
+#include <cmath>
+#include <string>
+
+std::string Temperatures::ComputeTemperatures(int n, int& closestToZero, std::istream& input)
+{
+    return std::to_string(0);
+}
---
+++ CodingGameSolutionsBackup/src/Temperatures/Temperatures.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <istream>
+#include <string>
+
+class Temperatures
+{
+public:
+    std::string ComputeTemperatures(int n, int& closestToZero, std::istream& input);
+};
