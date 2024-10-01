c5e62f1313855617354e5b83c8feb67d8f2386d9
JotaPe <jcamacho249@unab.edu.co>
2024-09-30 20:17:32 -0500

    [Add] Temperatures Exercise Solution

--- CodingGameSolutionsBackup/src/Temperatures/Temperatures.cpp
+++ CodingGameSolutionsBackup/src/Temperatures/Temperatures.cpp
@@ -1,8 +1,24 @@
-﻿#include "Temperatures.h"
+﻿// Exercise Link: https://www.codingame.com/ide/puzzle/temperatures
+
+#include "Temperatures.h"
 #include <cmath>
 #include <string>
 
 std::string Temperatures::ComputeTemperatures(int n, int& closestToZero, std::istream& input)
 {
-    return std::to_string(0);
+    if (n == 0)
+        return "0";
+
+    for (int i = 0; i < n; i++) 
+    {
+        int t; // a temperature expressed as an integer ranging from -273 to 5526
+        input >> t;
+
+        if (std::abs(t) < std::abs(closestToZero) || 
+            (std::abs(t) == std::abs(closestToZero) && t > closestToZero))
+        {
+            closestToZero = t;
+        }
+    }
+    return std::to_string(closestToZero);
 }
