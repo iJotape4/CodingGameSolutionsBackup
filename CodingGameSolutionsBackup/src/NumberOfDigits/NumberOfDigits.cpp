fee3a91ab8019a39290676832a3b6d1f9eb4e480
JotaPe <jcamacho249@unab.edu.co>
2024-10-02 21:01:14 -0500

    [Add] Puzzle: Number of digits - Solution

--- CodingGameSolutionsBackup/src/NumberOfDigits/NumberOfDigits.cpp
+++ CodingGameSolutionsBackup/src/NumberOfDigits/NumberOfDigits.cpp
@@ -6,5 +6,18 @@
 
 std::string NumberOfDigits::computeInput(int& n, int& k)
 {
-    return "2";
+    int c = 0, digitsCounter=0;
+    while(n>=c)
+    {
+        for(char ch : std::to_string(c))
+        {
+            int w = ch-'0';
+            // std::cerr << "hello";
+            if(w == k)
+                digitsCounter++;
+        }
+        c++;  
+    }
+    
+    return std::to_string(digitsCounter);
 }
