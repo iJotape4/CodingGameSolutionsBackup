3e7fa04efc84a0eb8003be0d7cc64eafb236c33d
Juan <jcamacho249@unab.edu.co>
2024-11-11 21:14:19 -0500

    [Add] HackerRank/FindThePoint Test02

--- CodingGameSolutionsBackup/src/HackerRank/FindThePoint/test_FindThePoint.cpp
+++ CodingGameSolutionsBackup/src/HackerRank/FindThePoint/test_FindThePoint.cpp
@@ -24,3 +24,23 @@ TEST_F(test_find_the_point, test01)
     std::string result = classFile.ComputeInput(setsAmount, sets);
     EXPECT_EQ(result, "2 2 \n 3 3");
 }
+
+TEST_F(test_find_the_point, test02)
+{
+    int setsAmount = 10;
+    std::vector<std::string> sets
+    {
+    "1 1 2 2",
+    "4 3 5 2",
+    "2 4 5 6",
+    "1 2 2 2",
+    "1 1 1 1",
+    "1 2 2 1",
+    "1 8 7 8",
+    "9 1 1 1",
+    "8 4 3 2",
+    "7 8 9 1",
+    };
+    std::string result = classFile.ComputeInput(setsAmount, sets);
+    EXPECT_EQ(result, "3 3 \n 6 1 \n 8 8 \n 3 2 \n 1 1 \n 3 0 \n 13 8 \n -7 1 \n -2 0 \n 11 -6");
+}
