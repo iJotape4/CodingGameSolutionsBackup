1520ef0872bf85bc441b8f58f3385a731e915273
Juan <jcamacho249@unab.edu.co>
2024-11-11 22:21:56 -0500

    [Add] Vectorial Math library

---
+++ CodingGameSolutionsBackup/src/ChatGPT/3DMath/VectorialMathLibrary.cpp
@@ -0,0 +1,17 @@
+﻿#include "VectorialMathLibrary.h"
+
+float VectorialMathLibrary::DotProduct(FVector vector1, FVector vector2, FVector center)
+{
+        FVector a = (vector1 -center ).Normalize();
+        FVector b = (vector2 -center ).Normalize();
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+VectorialMathLibrary::FVector VectorialMathLibrary::CrossProduct(FVector a, FVector b)
+{
+        FVector result = FVector::Zero();
+        result.x = a.y * b.z - a.z * b.y;
+        result.y = a.z * b.x - a.x * b.z;
+        result.z = a.x * b.y - a.y * b.x;
+        return result;
+}
---
+++ CodingGameSolutionsBackup/src/ChatGPT/3DMath/VectorialMathLibrary.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <cmath>
+
+class VectorialMathLibrary
+{
+public:
+    struct FVector
+    {
+        float x, y, z;
+        FVector(float InX, float InY, float InZ) : x(InX), y(InY), z(InZ) {}
+
+        float Magnitude() const { return sqrt((x * x) + (y * y) + (z * z)); }
+        FVector Normalize() const
+        {
+            float Mag = Magnitude();
+            if(Mag == 0)
+                return FVector(0.0f, 0.0f, 0.0f);
+            
+            return FVector(x / Mag, y / Mag, z / Mag);
+        }
+
+        FVector operator-(const FVector& Other) const { return FVector(x - Other.x, y - Other.y, z - Other.z); }
+        FVector operator+(const FVector& Other) const { return FVector(x + Other.x, y + Other.y, z + Other.z); }
+
+        static FVector Zero() { return FVector(0.0f, 0.0f, 0.0f); }
+        static FVector One() { return FVector(1.0f, 1.0f, 1.0f); }
+    };
+    
+public:
+    float DotProduct(FVector vector1, FVector vector2, FVector center = FVector::Zero());
+    FVector CrossProduct(FVector a, FVector b);
+};
