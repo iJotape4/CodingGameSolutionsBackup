0e5823deaa4f8be0eb24cc28c145d032acb467ea
Juan <jcamacho249@unab.edu.co>
2024-11-11 21:07:34 -0500

    [Add] HackerRank/FindThePoint puzzle

---
+++ CodingGameSolutionsBackup/src/HackerRank/FindThePoint/FindThePoint.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+
+class FindThePoint
+{
+public:
+    std::string ComputeInput(int setsAmount, std::vector<std::string> sets);
+};
---
+++ CodingGameSolutionsBackup/src/HackerRank/FindThePoint/test_FindThePoint.cpp
@@ -0,0 +1,26 @@
+﻿#include <gtest/gtest.h>
+#include "FindThePoint.h"
+
+class test_find_the_point : public ::testing::Test
+{
+protected:
+    FindThePoint classFile;
+    std::istringstream input;
+
+    void SetUp() override
+    {
+        
+    }
+};
+
+TEST_F(test_find_the_point, test01)
+{
+    int setsAmount = 2;
+    std::vector<std::string> sets
+    {
+        "0 0 1 1",
+        "1 1 2 2"
+    };
+    std::string result = classFile.ComputeInput(setsAmount, sets);
+    EXPECT_EQ(result, "2 2 \n 3 3");
+}
