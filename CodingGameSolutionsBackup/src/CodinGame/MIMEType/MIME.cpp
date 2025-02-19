939284065edeb73294c7918ce0be8964b4ed9645
Juan <jcamacho249@unab.edu.co>
2025-02-18 22:03:51 -0500

    [Add] Code for unknown types

--- CodingGameSolutionsBackup/src/CodinGame/MIMEType/MIME.cpp
+++ CodingGameSolutionsBackup/src/CodinGame/MIMEType/MIME.cpp
@@ -7,6 +7,13 @@ std::string MIME::ComputeInput(int elements, int elementsToAnalize, std::vector<
 {
     std:std::ostringstream result;
 
+    //Handle Unknown Types
+    while (elementsToAnalize > elements)
+    {
+        result << "UNKNOWN\n";
+        --elementsToAnalize;
+    }
+        
     for (int i = elementsToAnalize - 1; i >= 0; --i)
     {
         sets[i] = sets[i].substr(sets[i].find(" ") + 1);
@@ -16,5 +23,4 @@ std::string MIME::ComputeInput(int elements, int elementsToAnalize, std::vector<
     }
     
    return result.str();
-    
 }
